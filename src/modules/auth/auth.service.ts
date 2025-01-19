a3c4db8ef53f220b461b4b3c40515f423834cb45
VaheAA <vahe.abov@gmail.com>
2025-01-19 17:27:14 +0400

    create helpers

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,12 +4,9 @@ import {
   NotFoundException,
 } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
-import { randomBytes, scrypt as _scrypt } from 'crypto'
-import { promisify } from 'util'
 import { CreateUserDto } from '../users/dto/create-user.dto'
 import { JwtService } from '@nestjs/jwt'
-
-const scrypt = promisify(_scrypt)
+import { comparePassword, hashPassword } from '../../shared/utils'
 
 @Injectable()
 export class AuthService {
@@ -26,12 +23,10 @@ export class AuthService {
         `User with email ${data.email} already exists`,
       )
 
-    const salt = randomBytes(8).toString('hex')
-    const hash = (await scrypt(data.password, salt, 32)) as Buffer
-
-    const result = salt + '.' + hash.toString('hex')
-
-    return await this.usersService.create({ ...data, password: result })
+    return await this.usersService.create({
+      ...data,
+      password: await hashPassword(data.password),
+    })
   }
 
   async signIn(email: string, password: string) {
@@ -39,12 +34,9 @@ export class AuthService {
 
     if (!user) throw new NotFoundException(`User with email ${email} not found`)
 
-    const [salt, storedHash] = user.password.split('.')
+    const isValidPassword = await comparePassword(password, user.password)
 
-    const hash = (await scrypt(password, salt, 32)) as Buffer
-
-    if (hash.toString('hex') !== storedHash)
-      throw new BadRequestException(`Invalid credentials`)
+    if (!isValidPassword) throw new BadRequestException(`Invalid credentials`)
 
     const payload = { sub: user.id, username: user.name, email: user.email }
     const token = await this.jwtService.signAsync(payload)
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,8 +1,26 @@
 import { Request } from 'express'
+import { randomBytes, scrypt as _scrypt } from 'crypto'
+import { promisify } from 'util'
+
+const scrypt = promisify(_scrypt)
 
 function extractTokenFromHeader(request: Request): string | undefined {
   const [type, token] = request.headers.authorization?.split(' ') ?? []
   return type === 'Bearer' ? token : undefined
 }
 
-export { extractTokenFromHeader }
+async function hashPassword(password: string) {
+  const salt = randomBytes(8).toString('hex')
+  const hash = (await scrypt(password, salt, 32)) as Buffer
+  return salt + '.' + hash.toString('hex')
+}
+
+async function comparePassword(password: string, storedPassword: string) {
+  const [salt, storedHash] = storedPassword.split('.')
+
+  const hash = (await scrypt(password, salt, 32)) as Buffer
+
+  return hash.toString('hex') === storedHash
+}
+
+export { extractTokenFromHeader, hashPassword, comparePassword }
