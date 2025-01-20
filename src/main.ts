5980cf3ae5db297f3411ed59611b42d71dccb7d5
VaheAA <vahe.abov@gmail.com>
2025-01-20 14:49:21 +0400

    port

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import * as process from 'node:process'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.$PORT || 8080)
-
-  console.log(process.env.$PORT)
+  const port = parseInt(process.env.PORT) || 8080
+  await app.listen(port)
 }
 bootstrap()
